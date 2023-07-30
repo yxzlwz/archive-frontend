226f137b53906a3bc5b0d493ed3c82a836a931e6
yxzlwz <danny070601@aliyun.com>
2023-07-30 21:02:53 +0800

    [fix]

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://archive.hccserver.qdzx.icu:7996/api',
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, ''),
       },
